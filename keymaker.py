bac0fefe940103308acfd05c2ae47e41545e9802
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2020-12-04 15:40:26 +0200

    Solved for function get_square_index_chars

--- keymaker.py
+++ keymaker.py
@@ -1,11 +1,12 @@
 def shift_characters(word, shift):
-    result = ''
+    result = ""
     for i in range(len(word)):
         char = word[i]
         result += chr((ord(char) + shift - 97) % 26 + 97)
     return result
 
-print(shift_characters('abby', 5))
+
+print(shift_characters("abby", 5))
 
 
 def pad_up_to(word, shift, n):
@@ -15,19 +16,20 @@ def pad_up_to(word, shift, n):
         new_word += word
     return new_word[0:n]
 
-print(pad_up_to('abb', 5, 11))
- 
+
+print(pad_up_to("abb", 5, 11))
+
 
 def abc_mirror(word):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     omega = alphabet[::-1]
-    result = ''
+    result = ""
     for letter in word:
         result += omega[alphabet.index(letter)]
     return result
 
-    
-print(abc_mirror('abcd'))
+
+print(abc_mirror("abcd"))
 
 
 def create_matrix(word1, word2):
@@ -47,18 +49,21 @@ def zig_zag_concatenate(matrix):
 
 
 def rotate_right(word, n):
-    return word[-n:] + word[: - n]
+    return word[-n:] + word[:-n]
 
-print(rotate_right('abcdefgh', 3))    
 
+print(rotate_right("abcdefgh", 3))
 
 
 def get_square_index_chars(word):
-    """
-    >>> get_square_index_chars('abcdefghijklm')
-    'abej'
-    """
-    pass
+    result = []
+    for i in range(len(word)):
+        if (i ** 0.5) == int(i ** 0.5):
+            result.append(word[i])
+    return "".join(result)
+
+print(get_square_index_chars("abcdefghijklm"))
+
 
 
 def remove_odd_blocks(word, block_length):
@@ -91,6 +96,6 @@ def hash_it(word):
     return key
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     name = input("Enter your name! ").lower()
-    print(f'Your key: {hash_it(name)}')
+    print(f"Your key: {hash_it(name)}")
